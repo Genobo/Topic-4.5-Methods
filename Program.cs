5461c133d8c7d19b7d53ef6c41fc0e0083298122
Gabriel Lindsay <339516205@GoTVDSB.ca>
2023-11-08 10:32:45 -0500

    done

--- Program.cs
+++ Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Press enter to view art.");
+            Console.ReadLine();
+            DrawSpeeder();
+            Console.WriteLine();
+            Console.WriteLine("Knock knock");
+            Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Chickens");
+            Convert.ToString(Console.ReadLine());
+            Console.WriteLine("No, Owls make that sound. Chickens cluck");
+        }
+
+        public static void DrawSpeeder()
+        {
+            Console.WriteLine("              ____==========_______");
+            Console.WriteLine("   _--____   |    | \"\"  \" \"|       \\");
+            Console.WriteLine("  /  )8}  ^^^| 0  |  =     |  o  0  |");
         }
     }
 }
